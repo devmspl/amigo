a901995909b7c6cd2ac6849158e4ab90618701d3
iosmspl <ios.mspl@gmail.com>
2021-10-18 11:01:51 +0530

    first commit

---
+++ Amigo/ForgotPassVC.swift
@@ -0,0 +1,24 @@
+//
+//  ForgotPassVC.swift
+//  Amigo
+//
+//  Created by Macbook on 14/10/21.
+//
+
+import UIKit
+
+class ForgotPassVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func back_btn(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
