37fa3a1461414fb8d58d29fd573b6e5624124f6c
iosmspl <ios.mspl@gmail.com>
2021-11-09 12:21:20 +0530

    api

--- Amigo/ApiManager/ApisManage.swift
+++ Amigo/ApiManager/ApisManage.swift
@@ -6,11 +6,38 @@
 //
 
 import Foundation
+import Alamofire
+import MBProgressHUD
 
 class ApiManager{
     let shared = ApiManager()
     
     func signUp(model: CreateUserModel, completionHandler: @escaping (Bool) -> ()){
-        
+        if ReachabilityNetwork.isConnectedToNetwork(){
+            AF.request(API.signUp, method: .post, parameters: model, encoder: JSONParameterEncoder.default).responseJSON{
+                response in
+                switch(response.result){
+                case .success(let data):do{
+                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                    print(json)
+                    if response.response?.statusCode == 200{
+                        completionHandler(true)
+                    }else{
+                        print("Completion false")
+                    }
+                }catch{
+                    print(error.localizedDescription)
+                    completionHandler(false)
+                }
+                case .failure(let error): do{
+                    print("Error",error)
+                    completionHandler(false)
+                }
+                }
+                
+            }
+        }else{
+            
+        }
     }
 }
